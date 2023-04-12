6d163b41fef0cafae6c49e0de67edebf7e48795b
Kaden Hesse <kadenhesse@Kadens-MBP.lan>
2023-04-11 22:13:40 -0500

    speed up

--- search.py
+++ search.py
@@ -1,4 +1,7 @@
 from get_info import analyze_car
+from datetime import datetime
+from tqdm import tqdm
+from page import Page
 
 urls = [
     'https://www.ebay.com/itm/385540746588?hash=item59c404ed5c%3Ag%3APtQAAOSwzgBkNWTH&mkevt=1&mkcid=1&mkrid=711-53200-19255-0&campid=5337650957&customid=&toolid=10049',
@@ -7,9 +10,21 @@ urls = [
     'https://www.ebay.com/itm/166026384440?hash=item26a7f19438%3Ag%3AslYAAOSwPCNkNIQ8&mkevt=1&mkcid=1&mkrid=711-53200-19255-0&campid=5337650957&customid=&toolid=10049',
 ]
 
+long_breif = Page()
+start = datetime.now()
+for url in tqdm(urls):
+    try:
+        long_breif.press(analyze_car(url), end='\n\n')
 
-for url in urls:
-    
-    print(f'Analyzing car from url: {url}')
-    analyze_car(url)
-    print('\n\nPrepareing for next car\n\n\n')
+    except Exception as e:
+        long_breif.press(e)
+
+    long_breif.press('Preparing for next car...', end='\n\n')
+
+time = datetime.now() - start
+avg = time/len(urls)
+
+print(f'average time: {avg}')
+
+with open('long_breifing.txt' , 'w') as file:
+    file.write(long_breif.body)
